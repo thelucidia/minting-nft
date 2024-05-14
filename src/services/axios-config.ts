412c41d71915e10d7e555ed67de9993743e12b74
Smart-Dev00214 <smart.dev00214@gmail.com>
2024-05-14 22:25:23 +0200

    fixed origin axios header error

--- src/services/axios-config.ts
+++ src/services/axios-config.ts
@@ -4,7 +4,6 @@ const instance = axios.create({
   baseURL: 'https://mintingback-7fd686953800.herokuapp.com/api',
   headers: {
     'Content-Type': 'application/json',
-    Origin: 'http://localhost:5000',
     'Access-Control-Allow-Origin': '*',
   },
 });
