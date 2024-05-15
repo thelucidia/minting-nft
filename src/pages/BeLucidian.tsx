d91421d68fefe0b5faa62eb0906d1dee455d47a9
Smart-Dev00214 <smart.dev00214@gmail.com>
2024-05-15 21:08:27 +0200

    again7

--- src/pages/BeLucidian.tsx
+++ src/pages/BeLucidian.tsx
@@ -35,7 +35,7 @@ const BeLucidian: React.FC = () => {
                             <img src="/token/Group 625215.png" width="300px" alt="zombie2" />
                         </div>
                         <div className="mt-[10px]">
-                            <button onClick={() => {window.location.href="/minting"}} className="rounded-[12px] bg-cyan hover:bg-white py-[13px] px-[150px] text-center bg-no-repeat bg-contain uppercase text-black">
+                            <button className="rounded-[12px] bg-cyan hover:bg-white py-[13px] px-[150px] text-center bg-no-repeat bg-contain uppercase text-black">
                                 <h4 className="font-bold font-secondary">Claim</h4>
                             </button>
                         </div>
