d9dead753e1f998c6da9ac5af217263be38c735a
Smart-Dev00214 <smart.dev00214@gmail.com>
2024-05-15 20:30:43 +0200

    again5

--- src/pages/Minting.tsx
+++ src/pages/Minting.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Suspense} from "react";
 import { motion } from 'framer-motion';
 
 import MintingBanner from "../views/minting/MintingBanner";
@@ -8,20 +8,22 @@ import Video from "../views/home/Video";
 
 const Minting: React.FC = () => {
     return (
-        <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 1.2 }}
-            className="bg-black"
-        >
-            <section className="w-full h-auto overflow-hidden bg-bg">
-                {/* <MintingBanner />
-                <Airdrop />
-                <Benefits />
-                <Video /> */}
-            </section>
-        </motion.div>
+        <Suspense fallback={<div>waiting</div>}>
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 1.2 }}
+                className="bg-black"
+            >
+                <section className="w-full h-auto overflow-hidden bg-bg">
+                    <MintingBanner />
+                    <Airdrop />
+                    <Benefits />
+                    <Video />
+                </section>
+            </motion.div>
+        </Suspense>
     );
 };
 
