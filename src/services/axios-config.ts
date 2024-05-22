35e1ac84013e59f6202de4309b1580c8f9d65155
cannonballbs <cannon.running.bs@gmail.com>
2024-05-22 01:18:30 -0700

    change the backend service.

--- src/services/axios-config.ts
+++ src/services/axios-config.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  // baseURL: "https://mintingback-7fd686953800.herokuapp.com/api",
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://mintingback-7fd686953800.herokuapp.com/api",
+  // baseURL: "http://localhost:5000/api",
   headers: {
     "Authorization": 'Bearer HRKU-783c968b-90b5-4109-a31e-a52e64a850d2',
     'Accept': 'application/vnd.heroku+json; version=3',
