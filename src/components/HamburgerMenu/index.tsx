277c57d8286023513bdf0eac6c575cfd40c6fb47
cannonballbs <cannon.running.bs@gmail.com>
2024-05-24 11:42:24 -0700

    update the menu items when connecting wallet

--- src/components/HamburgerMenu/index.tsx
+++ src/components/HamburgerMenu/index.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import { MenuType } from '../../utils/types';
+import { useAccount } from 'wagmi';
 
 interface HamburgerMenuProps {
   className?: string;
@@ -10,15 +11,21 @@ interface HamburgerMenuProps {
 
 const HamburgerMenu = (props: HamburgerMenuProps) => {
   const { className, style, items, onItemClick } = props;
+  const { address } = useAccount();
+
   return (
     <div
       className={`${className || ''} flex flex-col justify-center gap-4 p-8 text-xl font-semibold text-white/80 bg-transparent bg-hamburger-menu bg-no-repeat bg-cover min-w-[310px] z-50`}
       style={style}
     >
-      <Link to={localStorage.getItem('isUser') ? '/connect-wallet' : '/create-id'}>
-        <span className="hover:text-[#0ED4FF] cursor-pointer px-4">Login Passport</span>
-      </Link>
-      <span className="w-4/5 h-px bg-white/40 px-4" />
+      {!address && (
+        <>
+          <Link to={localStorage.getItem('isUser') ? '/connect-wallet' : '/create-id'}>
+            <span className="hover:text-[#0ED4FF] cursor-pointer px-4">Login Passport</span>
+          </Link>
+          <span className="w-4/5 h-px bg-white/40 px-4" />
+        </>
+      )}
       {items.map((item, idx) =>
         item.title === '@splitter' ? (
           <span key={idx} className="w-4/5 h-px bg-white/40 px-4" />
