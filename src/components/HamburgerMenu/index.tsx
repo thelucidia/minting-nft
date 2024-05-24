bc6f6a1ac48b79e0103352039dc543886dd6dfe7
cannonballbs <cannon.running.bs@gmail.com>
2024-05-24 11:29:55 -0700

    update the hamburger menu

--- src/components/HamburgerMenu/index.tsx
+++ src/components/HamburgerMenu/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { MenuType } from '../../utils/types';
 
 interface HamburgerMenuProps {
@@ -11,14 +12,18 @@ const HamburgerMenu = (props: HamburgerMenuProps) => {
   const { className, style, items, onItemClick } = props;
   return (
     <div
-      className={`${className || ''} flex flex-col gap-4 p-8 text-xl font-semibold text-white/80 bg-transparent bg-hamburger-menu bg-no-repeat bg-cover min-w-[260px] z-50`}
+      className={`${className || ''} flex flex-col justify-center gap-4 p-8 text-xl font-semibold text-white/80 bg-transparent bg-hamburger-menu bg-no-repeat bg-cover min-w-[310px] z-50`}
       style={style}
     >
+      <Link to={localStorage.getItem('isUser') ? '/connect-wallet' : '/create-id'}>
+        <span className="hover:text-[#0ED4FF] cursor-pointer px-4">Login Passport</span>
+      </Link>
+      <span className="w-4/5 h-px bg-white/40 px-4" />
       {items.map((item, idx) =>
         item.title === '@splitter' ? (
-          <span key={idx} className="w-4/5 h-px bg-white/40" />
+          <span key={idx} className="w-4/5 h-px bg-white/40 px-4" />
         ) : (
-          <span key={idx} className="hover:text-[#0ED4FF] cursor-pointer" onClick={() => onItemClick(item.path)}>
+          <span key={idx} className="hover:text-[#0ED4FF] cursor-pointer px-4" onClick={() => onItemClick(item.path)}>
             {item.title}
           </span>
         ),
