a4bb352187a8600f3d10a6e1eca2deccf599cad5
cannonballbs <cannon.running.bs@gmail.com>
2024-05-27 05:24:25 -0700

    fix the mobile responsive of minting

--- src/views/minting/MintingBanner.tsx
+++ src/views/minting/MintingBanner.tsx
@@ -18,7 +18,7 @@ const MintingBanner: React.FC = () => {
   }, []);
 
   return (
-    <section className="w-full minting-banner min-h-screen mx-autorelative text-white flex md:items-center justify-center mt-20 xd:mt-0">
+    <section className="w-full minting-banner min-h-screen mx-autorelative text-white flex md:items-center justify-center mt-20 xl:mt-0">
       <div
         className={`h-screen bg-black fixed top-0 left-0 flex flex-col items-center justify-center  transition-all ease-in-out duration-300 ${trueLoading ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'} `}
       >
@@ -42,15 +42,15 @@ const MintingBanner: React.FC = () => {
         className="absolute bottom-[-300px] right-[-150px]"
       />
       <div
-        className="h-auto mx-auto bg-[url('/assets/minting/solidier_mobile.png')] xd:bg-none bg-no-repeat bg-contain bg-center"
+        className="h-auto mx-auto bg-[url('/assets/minting/solidier_mobile.png')] xl:bg-none bg-no-repeat bg-contain bg-center max-w-screen-2xl"
         style={{ transform: 'rotate(-180deg)' }}
       >
-        <div className="flex items-center pt-96">
-          <div className="hidden xd:block xd:w-[50%]">
+        <div className="flex items-center pt-96 xl:pt-16">
+          <div className="hidden xl:block xl:w-[50%]">
             <img src="/minting/Mask group.png" alt="mask group" />
           </div>
-          <div className="flex flex-col items-center w-full xd:w-[50%]">
-            <div className="xd:flex xd:flex-col xd:gap-4 w-[80%] mx-auto hidden">
+          <div className="flex flex-col items-center w-full xl:w-[50%]">
+            <div className="xl:flex xl:flex-col xl:gap-4 w-[80%] mx-auto hidden">
               <img src="/hero/text2.png" alt="text2" />
               <p>
                 Curse Of The Pharaoh is an action/adventure/survival game set in a post-apocalyptic world overrun by
@@ -58,7 +58,7 @@ const MintingBanner: React.FC = () => {
                 biomes.
               </p>
             </div>
-            <div className="flex justify-center gap-9 mt-12">
+            <div className="hidden sm:flex justify-center gap-4 md:gap-9 mt-12">
               <img
                 className="border-8 border-white/40 rounded-4xl"
                 src="/minting/Mask group (1).png"
@@ -78,9 +78,9 @@ const MintingBanner: React.FC = () => {
             <div className="flex items-center justify-center mt-12 w-full">
               <GradientButton content="Mint now" />
             </div>
-            <div className="flex flex-col xd:hidden gap-4 my-12 py-5 w-4/5">
+            <div className="flex flex-col xl:hidden gap-4 my-12 py-5 w-4/5">
               <img src="/hero/text2.png" alt="text2" />
-              <p>
+              <p className="md:text-2xl">
                 Curse Of The Pharaoh is an action/adventure/survival game set in a post-apocalyptic world overrun by
                 zombies. Collect weapon, bullet, and fuel props. Travel through open-world environment with different
                 biomes.
