7b90708980480f59464794ef18692467eed7332c
cannonballbs <cannon.running.bs@gmail.com>
2024-05-28 03:23:43 -0700

    update the axios config

--- src/services/axios-config.ts
+++ src/services/axios-config.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  // baseURL: "https://mintingbackend-887dd6dfeda3.herokuapp.com/api",
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://mintingbackend-887dd6dfeda3.herokuapp.com/api",
   headers: {
     "Authorization": 'Bearer HRKU-783c968b-90b5-4109-a31e-a52e64a850d2',
     'Accept': 'application/vnd.heroku+json; version=3',
